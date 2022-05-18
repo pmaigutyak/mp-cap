d74a0c7f5146d9c08b7209128adec50e309130b8
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-05-18 23:35:31 +0300

    Update static part.

--- cap/__init__.py
+++ cap/__init__.py
@@ -4,6 +4,42 @@ from django.conf.global_settings import DATE_INPUT_FORMATS
 
 def setup_settings(settings, **kwargs):
 
+    settings['CSS_COMPONENTS'] = {
+        'admin': [
+            'cap/components/bootstrap/bootstrap.min.css',
+            'cap/components/jquery-ui/jquery-ui.min.css',
+            'cap/components/font-awesome/font-awesome.min.css',
+            'cap/components/qtip2/jquery.qtip.min.css',
+            'cap/components/fancybox/jquery.fancybox.min.css',
+            'cap/components/bootstrap-treeview/bootstrap-treeview.min.css',
+            'cap/components/bootstrap-tagsinput/bootstrap-tagsinput.css'
+            'cap/components/select2/select2.css'
+        ],
+        **settings.get('CSS_COMPONENTS', {})
+    }
+
+    settings['JS_COMPONENTS'] = {
+        'admin': [
+            'cap/components/jquery.min.js',
+            'cap/components/jquery.form.min.js',
+            'cap/components/bootstrap/bootstrap.min.js',
+            'cap/components/bootstrap-treeview/bootstrap-treeview.min.js',
+            'cap/components/fancybox/jquery.fancybox.min.js',
+            'cap/components/qtip2/jquery.qtip.min.js',
+            'cap/components/bootstrap-tagsinput/bootstrap-tagsinput.js',
+            'jquery/ajax-csrf.js',
+            'cap/components/jquery-ui/jquery-ui.min.js',
+            'cap/components/jquery-ui/i18n/en.js',
+            'cap/components/jquery-ui/i18n/uk.js',
+            'cap/components/jquery-ui/i18n/ru.js',
+            'cap/components/select2/select2.js',
+            'cap/components/select2/i18n/uk.js.js',
+            'cap/components/select2/i18n/en.js.js',
+            'cap/components/select2/i18n/ru.js.js',
+        ],
+        **settings.get('JS_COMPONENTS', {})
+    }
+
     settings['INSTALLED_APPS'] += [
         app for app in [
             'cap.configs.SuitConfig',
