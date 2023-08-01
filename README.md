8b566c0cb677d14cf98a5f92d4dc9bc70ed79d4d
Pasha Maigutyak <pmaigutyak@gmail.com>
2023-08-01 16:52:34 +0300

    Simplify code.

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 
 Django admin app
-* uses django-suit app
-* automatically installs admin
-
-Installation:
-* install djrunner app
-* add `cap` to `INSTALLED_APPS`
-* add `ADMIN_MENU` setting instead of `SuitConfig.menu`
-* add `SITE_HEADER` setting to set admin title
--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 from setuptools import setup, find_packages
 
 
-version = '3.0.6'
+version = '3.1.0'
 url = 'https://github.com/pmaigutyak/mp-cap'
 
-with open('requirements.txt') as f:
-    requires = f.read().splitlines()
 
 setup(
     name='django-mp-cap',
@@ -19,5 +17,4 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
-    install_requires=requires
 )
